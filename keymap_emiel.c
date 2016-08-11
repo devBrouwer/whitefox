bfbcaa00d2f7914d627c928c5ba882f3a7b7d005
Emiel <eeymiel@devbrouwer.com>
2016-08-11 19:45:19 +0200

    modified keymap to add l_alt/l_win swap and disable winkey layers

--- keymap_emiel.c
+++ keymap_emiel.c
@@ -12,23 +12,23 @@
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
      * ,---------------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Ins| 16 keys
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|F12| 16 keys
      * |---------------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|Del| 15 keys
      * |---------------------------------------------------------------|
-     * | FN2  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU| 12k, 2k
+     * | FN5  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU| 12k, 2k
      * |---------------------------------------------------------------| 
-     * |  FN3   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |FN2| 1k, 13k
+     * |  FN2   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |FN2| 1k, 13k
      * |---------------------------------------------------------------|
-     * |Ctl |Gui |Alt |         Space         |Alt |FN2 |_|Lef|Dow|Rig| 6k, 3k
+     * |Ctl |Gui |Alt |         Space         |Alt |FN5 |_|Lef|Dow|Rig| 6k, 3k
      * `---------------------------------------------------------------'
      */
     [0] = KEYMAP( \
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, INS,\
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, F12,\
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,     DEL,\
-        FN2,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,     MPLY,\
-        FN3,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  FN2,\
-        LCTL,LGUI,LALT,               SPC,           RALT,FN2,     LEFT,DOWN,RGHT \
+        FN5,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,     MPLY,\
+        FN2,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  SLCK,\
+        LCTL,LGUI,LALT,               SPC,           RALT,FN5,     LEFT,DOWN,RGHT \
     ),
 	//----------------------------------------------------------------------------------------
 	/* Layer 1: Dvorak-layer
@@ -51,8 +51,35 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,SCLN, Q  , J  , K  , X  , B  , M  , W  , V  , Z  ,TRNS,    TRNS,TRNS, \
         TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,    TRNS,TRNS,TRNS  \
     ),
+	
 	//----------------------------------------------------------------------------------------
-	/* Layer 2: FN-layer
+	/* Shift Action Layer so shift-esc is ~
+    	*/
+	[2] = KEYMAP( \
+        GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,\
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS,TRNS, \
+        TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,    TRNS,TRNS,TRNS \
+    ),
+	//disable windows key
+	[3] = KEYMAP( \
+        TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,\
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS,TRNS, \
+        TRNS,NO,TRNS,               TRNS,          TRNS,TRNS,    TRNS,TRNS,TRNS \
+    ),
+	//swap windows and alt
+	[4] = KEYMAP( \
+	TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,\
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS,TRNS, \
+        TRNS,LALT,LGUI,               TRNS,          TRNS,TRNS,    TRNS,TRNS,TRNS \
+    ),
+	//----------------------------------------------------------------------------------------
+	/* FN-layer
      * ,---------------------------------------------------------------.
      * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|VoM|Vo-|Vo+|
      * |---------------------------------------------------------------|
@@ -62,37 +89,18 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------|
      * |         |   |   |   |   |MPRV|MPLY|MNXT|   |   |      | PU|FN1|
      * |---------------------------------------------------------------|
-     * |    |    |    |                    |   |   |   |___| HM| PD|END|
+     * |    |FN3|FN4|                    |   |   |   |___| HM| PD|END|
      * `---------------------------------------------------------------'
      */
-    [2] = KEYMAP( \
+    [5] = KEYMAP( \
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, MUTE,VOLD,VOLU,\
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGDN,PGUP,END ,TRNS,TRNS,TRNS,DEL ,    TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LEFT,DOWN, UP ,RGHT,TRNS,TRNS,TRNS,    TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    PGUP,FN1, \
-        TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,    HOME,PGDN,END  \
+        TRNS,FN3,FN4,               TRNS,          TRNS,TRNS,    HOME,PGDN,END  \
     ),
-	//----------------------------------------------------------------------------------------
-	/* Layer 3: Shift Action Layer so shift-esc is ~
-     * ,---------------------------------------------------------------.
-     * | ` |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-     * |---------------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |   |
-     * |---------------------------------------------------------------|
-     * |      |   |   |   |   |   |   |   |   |   |   |   |        |   |
-     * |---------------------------------------------------------------|
-     * |        |   |   |   |   |   |   |   |   |   |   |      |   |   |
-     * |---------------------------------------------------------------|
-     * |    |    |    |                    |   |   |   |___|   |   |   |
-     * `---------------------------------------------------------------'
-     */
-	[3] = KEYMAP( \
-        GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,\
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS,TRNS, \
-        TRNS,TRNS,TRNS,               TRNS,          TRNS,TRNS,    TRNS,TRNS,TRNS \
-    ),
+
+
 	
 	
 };
@@ -100,6 +108,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t fn_actions[] = {
     [0] = ACTION_LAYER_MODS(0, MOD_LCTL), //UNUSED
 	[1] = ACTION_LAYER_TOGGLE(1),
-	[2] = ACTION_LAYER_MOMENTARY(2),
-	[3] = ACTION_LAYER_MODS(3, MOD_LSFT),
+	[2] = ACTION_LAYER_MODS(2, MOD_LSFT),
+	[3] = ACTION_LAYER_TOGGLE(3),
+	[4] = ACTION_LAYER_TOGGLE(4),
+	[5] = ACTION_LAYER_MOMENTARY(5)
 };
